ed4a6982cf2acdd4c8552a3a31464425521c5d94
Marin Muso <mmuso000@citymail.cuny.edu>
2019-12-04 09:18:17 -0500

    part 2 of day 4 with tests

--- day4/find_passwords.py
+++ day4/find_passwords.py
@@ -3,23 +3,36 @@
 PUZZLE_RANGE = range(372037, 905157)
 
 
+def check_never_decreasing(num):
+    num = str(num)
+    return all(a <= b for a, b in zip(num, num[1:]))
+  
+    
 def check_adj_digits(num):
     num = str(num)
     return any(a == b for a, b in zip(num, num[1:]))
 
 
-def check_never_decreasing(num):
+def check_group_digits(num):
     num = str(num)
-    return all(a <= b for a, b in zip(num, num[1:]))
+    for digit in num:
+        if num.count(digit) == 2:
+            return True
+    return False
+        
 
-
-def possible_password(range_nums=PUZZLE_RANGE):
+def possible_passwords(part=1, range_nums=PUZZLE_RANGE):
+    check = {
+        1: check_adj_digits,
+        2: check_group_digits
+    }
     possible_pws = 0
     for num in range_nums:
-        if check_adj_digits(num) and check_never_decreasing(num):
-            possible_pws += 1
+        if check_never_decreasing(num) and check[part](num):
+            possible_pws += 1 
     return possible_pws
 
 
 if __name__ == '__main__':
-    print(f'Part 1: {possible_password()}')
+    print(f'Part 1: {possible_passwords(part=1)}')
+    print(f'Part 2: {possible_passwords(part=2)}')
--- day4/test_find_passwords.py
+++ day4/test_find_passwords.py
@@ -1,6 +1,6 @@
 import pytest
 
-from find_passwords import check_adj_digits, check_never_decreasing
+from find_passwords import check_adj_digits, check_never_decreasing, check_group_digits
 
 
 @pytest.mark.parametrize("test_input, expected", [
@@ -8,7 +8,6 @@ from find_passwords import check_adj_digits, check_never_decreasing
     (123456, False),
     (891000, True),
     (657710, True)
-
 ])
 def test_check_adj_digits(test_input, expected):
     assert check_adj_digits(test_input) == expected
@@ -22,4 +21,13 @@ def test_check_adj_digits(test_input, expected):
 ])
 def test_check_never_decreasing(test_input, expected):
     assert check_never_decreasing(test_input) == expected
-    
+
+
+@pytest.mark.parametrize("test_input, expected", [
+    (112233, True),
+    (123444, False),
+    (111122, True),
+    (221111, True),
+])
+def test_check_group_digits(test_input, expected):
+    assert check_group_digits(test_input) == expected
